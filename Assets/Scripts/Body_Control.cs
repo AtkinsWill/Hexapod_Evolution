54a9c92635b88f966949bb36ed585fc3770979b5
AtkinsWill <will.atkins13@gmail.com>
2023-04-26 15:50:13 +0100

    changed movement to be based on articulation bodies instead of local transform rotation, torso now controls all joints in the articulation body through articulationBody.setDriveTargets with a list of all floats. The fixed joint in the upper leg is included in the DoF count but not past the knee, so only one joint value needs to be skipped

--- Assets/Scripts/Body_Control.cs
+++ Assets/Scripts/Body_Control.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Body_Control : MonoBehaviour
 {
+    public List<float> fullJointTargets;
+    public List<float> actualJointTargets;
+    public GameObject[] legs;
+    public int DoF;
+    public ArticulationBody artiulationBody;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        artiulationBody = GetComponent<ArticulationBody>();
+        DoF = 30;
+        fullJointTargets = new List<float>(DoF);
+        for (int i = 0; i < DoF; i++)
+        {
+            fullJointTargets.Add(0.0f);
+        }
+        actualJointTargets = new List<float>(3);
+        for (int i = 0; i < 3; i++)
+        {
+            actualJointTargets.Add(0.0f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int legJoint = 6; legJoint < DoF; legJoint += 4)
+        {
+            Debug.Log(legJoint);
+            fullJointTargets[legJoint] = actualJointTargets[0];
+            fullJointTargets[legJoint+1] = actualJointTargets[1];
+            fullJointTargets[legJoint+3] = actualJointTargets[2];
+
+        }
         
+        artiulationBody.SetDriveTargets(fullJointTargets);
+ 
     }
 }
--- Assets/Scripts/Leg_Control.cs
+++ Assets/Scripts/Leg_Control.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class Leg_Control : MonoBehaviour
 {
 
-    public Vector3 jointAngleTargets;
+    public List<float> jointAngleTargets;
     public Vector3 limbDimensions;
     //public GameObject hip;
     public GameObject knee;
-
+    Vector3 hipEuler;
+    Vector3 kneeEuler;
 
     // Start is called before the first frame update
     void Start()
     {
-        jointAngleTargets = new Vector3(0.0f, 0.0f, 0.0f);
-        limbDimensions = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        jointAngleTargets += new Vector3(0.1f, 0.1f, 0.1f);
-        Vector3 hipEuler = new Vector3(transform.localRotation.x + jointAngleTargets[0], transform.localRotation.y + jointAngleTargets[1], transform.localRotation.z);
-        Vector3 kneeEuler = new Vector3(jointAngleTargets[2], 0, 0);
+        //jointAngleTargets
+        /*
+        var hipRollMotor = GetComponent<ArticulationBody>().xDrive;
+        var hipYawMotor = GetComponent<ArticulationBody>().yDrive;
+        var kneeRollMotor = knee.transform.GetComponent<ArticulationBody>().xDrive;
+        if (kneeRollMotor != null)
+        {
+            kneeRollMotor.target = jointAngleTargets[2];
+        }
+        else
+        {
+            Debug.Log("Problem");
+        }
+        
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(hipEuler), 0.5f);
-        knee.transform.localRotation = Quaternion.RotateTowards(knee.transform.localRotation, Quaternion.Euler(kneeEuler), 0.5f);
+        hipRollMotor.target = jointAngleTargets[0];
+        hipYawMotor.target = jointAngleTargets[1];
+        
 
+        gameObject.GetComponent<ArticulationBody>().xDrive = hipRollMotor;
+        gameObject.GetComponent<ArticulationBody>().yDrive = hipYawMotor;
+        knee.GetComponent<ArticulationBody>().yDrive = kneeRollMotor;
 
-
+        */
+        GetComponent<ArticulationBody>().SetDriveTargets(jointAngleTargets);
     }
 }
